5cdf39f2b001d99e5e0f4d0c448f43f964a83853
code-testing <35744725+code-testing@users.noreply.github.com>
2020-12-22 17:40:23 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
 const myHeading = document.querySelector('h3');
 myHeading.textContent = "THIS IS MY JAVASCRIPT TITLE";
+myHeading.fadeIn('slow');
+
+/*
+
+
+function fadeIn(myHeading, display){
+
+	myHeading.opacity = 0;
+	myHeading.style.display = display || "block";
+	(function fade() {
+		var val = parseFloat(myHeading.style.opacity);
+		if (!(val +=.1) >= 1)) {
+			myHeading.style.opacity = val;
+			requestAnimationFrame(fade);
+		}
+	}
+};
+*/
